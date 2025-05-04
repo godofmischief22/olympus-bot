93b110de761d710829aa0efedfa14ee22215c27c
godofmischief22 <aadionline23@gmail.com>
2025-05-04 20:32:04 +0530

    Add files via upload

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,9 @@
 import os
 
 TOKEN = os.environ.get("TOKEN")
-NAME = "Olympus"
-server = "https://discord.com/invite/odx"
-ch = "https://discord.com/channels/699587669059174461/1271825678710476911"
-OWNER_IDS = [213347081799073793, 1070619070468214824]
-BotName = "Olympus"
-serverLink = "https://discord.com/invite/odx"
+NAME = "Sentinel"
+server = "https://discord.gg/xXqPVtZV7h"
+ch = "https://discord.com/channels/1368115543575498876"
+OWNER_IDS = [1131806691969728593]
+BotName = "Sentinel"
+serverLink = "https://discord.gg/xXqPVtZV7h"
