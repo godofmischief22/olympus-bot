3ea0ef67007499f8491c7e83f382e7499d953b4b
godofmischief22 <aadionline23@gmail.com>
2025-05-04 21:41:28 +0530

    Update help.py

--- cogs/commands/help.py
+++ cogs/commands/help.py
@@ -151,22 +151,32 @@ embed.add_field(
 )
 
 embed.add_field(
-    text="**Use buttons to swap pages & menu to select help pages. Need help? [Contact Support.](https://discord.gg/xXqPVtZV7h)**"
+    name="**Use buttons to swap pages & menu to select help pages. Need help? [Contact Support.](https://discord.gg/xXqPVtZV7h)**",
+    value="\u200b",  # Empty value for better formatting
+    inline=False
 )
 
-    embed.set_footer(
-      text=f"Requested By {self.context.author}",
-      icon_url=self.context.author.avatar.url if self.context.author.avatar else self.context.author.default_avatar.url
-    )
-    embed.set_author(name=self.context.author, icon_url=self.context.author.avatar.url if self.context.author.avatar else self.context.author.default_avatar.url)
+embed.set_footer(
+    text=f"Requested By {self.context.author}",
+    icon_url=self.context.author.avatar.url if self.context.author.avatar else self.context.author.default_avatar.url
+)
+embed.set_author(
+    name=self.context.author,
+    icon_url=self.context.author.avatar.url if self.context.author.avatar else self.context.author.default_avatar.url
+)
 
-    #embed.timestamp = discord.utils.utcnow()
-    view = vhelp.View(mapping=mapping,
-                          ctx=self.context,
-                          homeembed=embed,
-                          ui=2)
-    await asyncio.sleep(0.5)
-    await ok.edit(embed=embed,view=view)
+# embed.timestamp = discord.utils.utcnow()
+
+view = vhelp.View(
+    mapping=mapping,
+    ctx=self.context,
+    homeembed=embed,
+    ui=2
+)
+
+await asyncio.sleep(0.5)
+await ok.edit(embed=embed, view=view)
+
 
 
 
