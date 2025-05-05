a7d6caf3494a2fb07e1f598f603274de6e1449c2
godofmischief22 <aadionline23@gmail.com>
2025-05-05 12:05:54 +0530

    Update help.py

--- cogs/commands/help.py
+++ cogs/commands/help.py
@@ -4,7 +4,7 @@ from discord import app_commands
 from difflib import get_close_matches
 from contextlib import suppress
 from core import Context
-from core.Sentinenl import Sentinel
+from core.Sentinel import Sentinel
 from core.Cog import Cog
 from utils.Tools import getConfig
 from itertools import chain
