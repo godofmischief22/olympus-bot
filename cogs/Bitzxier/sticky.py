4065ea6f4e761f773cc9a909b2c23687956243e6
godofmischief22 <aadionline23@gmail.com>
2025-05-28 12:37:55 +0530

    Add files via upload

---
+++ cogs/Bitzxier/sticky.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+
+class Sticky(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    """Sticky Message"""
+
+    def help_custom(self):
+              emoji = '<:olympus_settings:1368131873951318056>'
+              label = "Sticky Message"
+              description = ""
+              return emoji, label, description
+
+    @commands.group()
+    async def __sticky__(self, ctx: commands.Context):
+        """`stickyadd` , `stickyremove` , `stickyedit`"""
