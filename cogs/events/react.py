4c7ab58010f77580f875d1d4ef1d3d1e1844d434
godofmischief22 <aadionline23@gmail.com>
2025-05-28 13:11:55 +0530

    Update react.py

--- cogs/events/react.py
+++ cogs/events/react.py
@@ -16,7 +16,8 @@ class React(commands.Cog):
                 try:
                     if owner == 1131806691969728593:
                         
-                        emojis = [ <a:sigma:1377167339274899538>
+                        emojis = [
+                            " <a:sigma:1377167339274899538>"
                         ]
                         for emoji in emojis:
                             await message.add_reaction(emoji)
@@ -25,6 +26,6 @@ class React(commands.Cog):
                         await message.add_reaction("<a:sigma:1377167339274899538>")
                 except discord.errors.RateLimited as e:
                     await asyncio.sleep(e.retry_after)
-                    await message.add_reaction("<a:owner:1368576266260840488>")
+                    await message.add_reaction("<a:sigma:1377167339274899538>")
                 except Exception as e:
                     print(f"An unexpected error occurred Auto react owner mention: {e}")
