4f288be241ff4dfccb399d1bc975c50ecc1acfb5
godofmischief22 <aadionline23@gmail.com>
2025-05-31 21:26:33 +0530

    Update Logging.py

--- cogs/Bitzxier/Logging.py
+++ cogs/Bitzxier/Logging.py
@@ -1,1 +1,20 @@
+import discord
+from discord.ext import commands
 
+
+class _voice(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    """Logging commands"""
+  
+    def help_custom(self):
+		      emoji = '<:olympus_mic:1368448688346759249>'
+		      label = "Logging Commands"
+		      description = ""
+		      return emoji, label, description
+
+    @commands.group()
+    async def __Loggging__(self, ctx: commands.Context):
+        """
+        `setuplogs`"""
